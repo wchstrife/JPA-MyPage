efd208f9a70e275fa1bc6f4e6408c34707360ca9
wangchenghao <1021717079@qq.com>
2017-11-08 21:15:48 +0800

    Initial commit

---
+++ src/main/java/com/wchstrife/entity/Student.java
@@ -0,0 +1,88 @@
+package com.wchstrife.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * Created by wangchenghao on 2017/11/8.
+ */
+@Entity
+@Table(name = "student")
+public class Student implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid")
+    @Column(name = "id", columnDefinition = "varchar(64) binary")
+    private String id;
+
+    @Column(name = "name", length = 128)
+    private String name;
+
+    @Column(name = "name")
+    private int age;
+
+    @Column(name = "idcard")
+    private String idCard;
+
+    @Column(name = "school_name")
+    private String schoolName;
+
+    public Student() {
+    }
+
+    public Student(String name, int age, String idCard, String schoolName) {
+        this.name = name;
+        this.age = age;
+        this.idCard = idCard;
+        this.schoolName = schoolName;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getIdCard() {
+        return idCard;
+    }
+
+    public void setIdCard(String idCard) {
+        this.idCard = idCard;
+    }
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+}
